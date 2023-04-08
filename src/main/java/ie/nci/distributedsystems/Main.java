07e8e11482256e6dc7f25bc76165443ba14fae33
Donal Hanway <x22148957@student.ncirl.ie>
2023-04-08 17:41:42 +0100

    Environment and directory set up for project

--- src/main/java/ie/nci/distributedsystems/Main.java
+++
@@ -1,7 +0,0 @@
-package ie.nci.distributedsystems;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ src/main/java/ie/nci/distributedsystems/client/ControllerGUI.java
@@ -0,0 +1,9 @@
+package ie.nci.distributedsystems.client;
+
+public class ControllerGUI
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
